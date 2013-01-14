8984d0fe85bcb3c242f3c73cf98b58a610312f85
waitwind <jinpujun@gmail.com>
2013-01-14 22:56:56 +0800

    web coding

--- webapp/src/main/webapp/js/core.js
+++ webapp/src/main/webapp/js/core.js
@@ -6,8 +6,28 @@ function XmlReader(text) {
 		this.xmlDom = parser.parseFromString(text, "text/xml");
 	}
 	else { // Internet Explorer
-		this.xmlDom = new ActiveXObject("Microsoft.XMLDOM");
+		this.xmlDom = new ActiveXObject("Msxml2.DOMDOCUMENT");
 		this.xmlDom.async = false;
 		this.xmlDom.loadXML(text); 
     } 
 }
+
+XmlReader.prototype.toXml = function() {
+	var str = "";
+	if(window.DOMParser) { 
+		str = new XMLSerializer().serializeToString(this.xmlDom.documentElement);
+	}
+	else {
+		str = this.xmlDom.xml;
+	}
+	return str;
+}
+
+
+
+
+
+
+
+
+
