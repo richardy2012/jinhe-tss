e13a97db0a83d414e9269cfc2ab3b3269df9c952
waitwind <jinpujun@gmail.com>
2013-01-24 23:21:34 +0800

    xml operation define

--- webapp/src/main/webapp/js/core.js
+++ webapp/src/main/webapp/js/core.js
@@ -10,6 +10,70 @@ function XmlReader(text) {
 		this.xmlDom.async = false;
 		this.xmlDom.loadXML(text); 
     } 
+
+	this.documentElement = this.xmlDom.documentElement;
+}
+
+XmlReader.prototype.createElement = function(name) {
+	var node = this.xmlDom.createElement(name);
+	return new XmlNode(node);
+}
+
+XmlReader.prototype.createCDATA = function(data) {
+	var xmlNode;
+	data = String(data).convertCDATA();
+	if(window.DOMParser) {
+		var tempReader = new XmlReader("<root><![CDATA[" + data + "]]></root>");
+		var xmlNode =  new XmlNode(tempReader.documentElement.firstChild);
+	}
+	else {
+		xmlNode = XmlNode(this.xmlDom.createCDATASection(data));
+	}
+	return xmlNode;
+}
+
+ XmlReader.prototype.createElementCDATA = function(name, data) {
+	var xmlNode   = this.createElement(name);
+	var cdataNode = this.createCDATA(data);
+	xmlNode.appendChild(cdataNode);
+	return xmlNode;
+}
+
+XmlReader.prototype.load = function(url, async) {
+	if(window.DOMParser) {
+
+	}
+	else {
+		var thisObj = this;
+		this.xmlDom.async = async;
+		this.xmlDom.onreadystatechange = function() {
+			if(thisObj.xmlDom.readyState == 4) {
+				var onloadType = typeof(thisObj.onload);
+				try
+				{
+					if(onloadType == "function") {
+						thisObj.onload();
+					} 
+					else if(onloadType == "string") {
+						eval(thisObj.onload);
+					}
+				}
+				catch (e)
+				{
+				}
+			}
+		}
+		this.xmlDom.load(url);
+	}
+
+	this.documentElement = this.xmlDom.documentElement;
+}
+
+XmlReader.prototype.toString = function() {
+	var str = [];
+	str[str.length] = "[XmlReader Object]";
+	str[str.length] = "xml:" + this.toXml();
+	return str.join("\r\n");
 }
 
 XmlReader.prototype.toXml = function() {
@@ -118,11 +182,128 @@ XmlNode.prototype.cloneNode = function(deep) {
 	}
 }
 
+XmlNode.prototype.getParent = function() {
+	var xmlNode = null;
+	if( this.node.parentNode != null ) {
+		xmlNode = new XmlNode(this.node.parentNode);
+	}
+	return xmlNode;
+}
+
+XmlNode.prototype.removeNode = function() {
+	var parentNode = this.node.parentNode;
+	if(parentNode != null) {
+		parentNode.removeChild(this.node);
+	}
+}
+
+XmlNode.prototype.selectSingleNode = function(xpath) {
+	var xmlNode = null;
+	if(window.DOMParser) {
+		var xPath;   
+        var xresult = this.ownerDocument.evaluate(xPath, this.node  
+            , null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);   
+        if(xresult && xresult.snapshotLength > 0) {   
+           return xresult.snapshotItem(0);   
+        }     
+	} 
+	else {
+		var node = this.node.selectSingleNode(xpath);
+		if(node != null) {
+			xmlNode = new XmlNode(node);
+		}
+	}
+	return xmlNode;
+}
+
+XmlNode.prototype.appendChild = function(xmlNode) {
+	this.node.appendChild(xmlNode.node);
+
+	this.nodeValue = this.node.nodeValue;
+	this.text = this.node.text;
+	this.firstChild = this.node.firstChild;
+	this.lastChild = this.node.lastChild;
+	this.childNodes = this.node.childNodes;
+}
+
+XmlNode.prototype.getFirstChild = function() {
+	if(this.firstChild) {
+		return new XmlNode(this.firstChild);
+	}
+	return null;
+}
+
+XmlNode.prototype.getLastChild = function() {
+	if(this.lastChild) {
+		return new XmlNode(this.lastChild);
+	}
+	return null;
+}
+
+// 交换子节点
+XmlNode.prototype.replaceChild = function(newNode, oldNode) {
+	var oldParent = oldNode.getParent();
+	if(oldParent != null && oldParent.equals(this)) {
+		try
+		{
+			this.node.replaceChild(newNode.node, oldNode.node);
+		}
+		catch (e)
+		{
+		}
+	}
+}
+		
+
+// 交换节点
+XmlNode.prototype.swapNode = function(xmlNode) {
+	var parentNode = this.getParent();
+	if(parentNode != null) {
+		parentNode.replaceChild(xmlNode, this);
+	}
+}
+
+/*
+ *	获取前一个兄弟节点
+ */
+XmlNode.prototype.getPrevSibling = function() {
+	var xmlNode = null;
+	if(null!=this.node.previousSibling) {
+		xmlNode = new XmlNode(this.node.previousSibling);
+	}
+	return xmlNode;
+}
+
+/*
+ * 获取后一个兄弟节点
+ */
+XmlNode.prototype.getNextSibling = function() {
+	if(this.node.nextSibling != null) {
+		return new XmlNode(this.node.nextSibling);
+	}
+	return null;
+}
+
+XmlNode.prototype.equals = function(xmlNode) {
+	return null != xmlNode && this.node == xmlNode.node;
+}
 
 
+XmlNode.prototype.toString = function() {
+	var str = [];
+	str[str.length] = "[XmlNode]";
+	str[str.length] = "nodeName:" + this.nodeName;
+	str[str.length] = "nodeType:" + this.nodeType;
+	str[str.length] = "nodeValue:" + this.nodeValue;
+	str[str.length] = "xml:" + this.toXml();
+	return str.join("\r\n");
+}
 
-
-
-
-
-
+XmlNode.prototype.toXml = function() {
+	if(window.DOMParser) {
+		return new XMLSerializer().serializerToString(this.node);
+	}
+	else {
+		return this.node.xml
+	}
+}
