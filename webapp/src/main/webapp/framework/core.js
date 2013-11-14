75fff4c15cc0d3e9b63e72892e8de9a9f2a01046
Jon.King <jinpujun@gmail.com>
2013-11-14 19:59:42 +0800

    resize function finished

--- webapp/src/main/webapp/framework/core.js
+++ webapp/src/main/webapp/framework/core.js
@@ -832,64 +832,39 @@ Element.insertHtml = function(where, el, html) {
  * 参数：	Object:element   要拖动改变宽度的HTML对象
  */
 Element.attachColResize = function(element) {
-	var handle = document.createElement("DIV"); // 拖动条
-	handle.style.cssText = "cursor:col-resize;position:absolute;overflow:hidden;float:right;top:0px;right:0px;width:3px;height:100%;z-index:3;filter:alpha(opacity:80);opacity:80;background:red;";
-	element.appendChild(handle);
-
-	var mouseStart  = 0;  // 鼠标起始位置
-	var handleStart = 0;  // 拖动条起始位置
-
-	handle.onmousedown = function(ev) {
-		var oEvent = ev || event;
-		mouseStart  = oEvent.clientX;
-		handleStart = handle.offsetLeft;
-
-		if (handle.setCapture) {
-			handle.onmousemove = doDrag;
-			handle.onmouseup = stopDrag;
-			handle.setCapture();
-		} else {
-			document.addEventListener("mousemove", doDrag, true);
-			document.addEventListener("mouseup", stopDrag, true);
-		}
-	};
-
-	function doDrag(ev) {
-		var oEvent = ev || event;
-		var _width = oEvent.clientX - mouseStart + handleStart + handle.offsetWidth;
-
-		if (_width < handle.offsetWidth) {
-			_width = handle.offsetWidth;
-		} 
-		else if (_width > document.documentElement.clientWidth - element.offsetLeft) {
-			_width = document.documentElement.clientWidth - element.offsetLeft - 2; // 防止拖出窗体外
-		}
-		element.style.width = _width + "px";
-	};
-
-	function stopDrag() {
-		if (handle.releaseCapture) {
-			handle.onmousemove = handle.onmouseup = null;
-			handle.releaseCapture();
-		} else {
-			document.removeEventListener("mousemove", doDrag, true);
-			document.removeEventListener("mouseup", stopDrag, true);
-		}
-	};
+	 Element.attachResize(element, "col");
 }
 
+/*
+ * 控制对象拖动改变高度
+ * 参数：	Object:element   要拖动改变高度的HTML对象
+ */
 Element.attachRowResize = function(element) {
+	 Element.attachResize(element, "row");
+}
+ 
+
+Element.attachResize = function(element, type) {
 	var handle = document.createElement("DIV"); // 拖动条
-	handle.style.cssText = "cursor:row-resize;position:absolute;overflow:hidden;left:0px;bottom:0px;width:100%;height:3px;z-index:3;filter:alpha(opacity:0);opacity:0;";
+	if (type == "col") {
+		handle.style.cssText = "cursor:col-resize;position:absolute;overflow:hidden;float:right;top:0px;right:0px;width:3px;height:100%;z-index:3;filter:alpha(opacity:80);opacity:80;background:red;";
+	} else if(type == "row") {
+		handle.style.cssText = "cursor:row-resize;position:absolute;overflow:hidden;left:0px;bottom:0px;width:100%;height:3px;z-index:3;filter:alpha(opacity:0);opacity:0;";
+	} else {
+		handle.style.cssText = "cursor:nw-resize;position:absolute;overflow:hidden;right:0px;bottom:0px;width:8px;height:8px;z-index:3;background:#99CC00";
+	}
+	
 	element.appendChild(handle);
 
-	var mouseStart  = 0;  // 鼠标起始位置
-	var handleStart = 0;  // 拖动条起始位置
+	var mouseStart  = {x:0, y:0};  // 鼠标起始位置
+	var handleStart = {x:0, y:0};  // 拖动条起始位置
 
 	handle.onmousedown = function(ev) {
 		var oEvent = ev || event;
-		mouseStart  = oEvent.clientY;
-		handleStart = handle.offsetTop;
+		mouseStart.x  = oEvent.clientX;
+		mouseStart.y  = oEvent.clientY;
+		handleStart.x = handle.offsetLeft;
+		handleStart.y = handle.offsetTop;
 
 		if (handle.setCapture) {
 			handle.onmousemove = doDrag;
@@ -903,15 +878,30 @@ Element.attachRowResize = function(element) {
 
 	function doDrag(ev) {
 		var oEvent = ev || event;
-		var _height = oEvent.clientY - mouseStart + handleStart + handle.offsetHeight;
 
-		if (_height < handle.offsetHeight) {
-			_height = handle.offsetHeight;
-		} 
-		else if (_height > document.documentElement.clientHeight - element.offsetTop) {
-			_height = document.documentElement.clientHeight - element.offsetTop - 2; // 防止拖出窗体外
+		// 水平移动距离
+		if (type == "col" || type == null) {
+			var _width = oEvent.clientX - mouseStart.x + handleStart.x + handle.offsetWidth;
+			if (_width < handle.offsetWidth) {
+				_width = handle.offsetWidth;
+			} 
+			else if (_width > document.documentElement.clientWidth - element.offsetLeft) {
+				_width = document.documentElement.clientWidth - element.offsetLeft - 2; // 防止拖出窗体外
+			}
+			element.style.width = _width + "px";
 		}
-		element.style.height = _height + "px";
+
+		// 垂直移动距离
+		if (type == "row" || type == null) {
+			var _height = oEvent.clientY - mouseStart.y + handleStart.y + handle.offsetHeight;
+			if (_height < handle.offsetHeight) {
+				_height = handle.offsetHeight;
+			} 
+			else if (_height > document.documentElement.clientHeight - element.offsetTop) {
+				_height = document.documentElement.clientHeight - element.offsetTop - 2; // 防止拖出窗体外
+			}
+			element.style.height = _height + "px";
+		}
 	};
 
 	function stopDrag() {
@@ -925,10 +915,63 @@ Element.attachRowResize = function(element) {
 	};
 }
 
-Element.attachResize = function(obj) {
+/*
+ * 拖动对象，改变其位置
+ * 参数：	Object:element   要拖动的HTML对象
+ */
+Element.moveable = function(element) {
+	var handle = element.getElementsByTagName("h2")[0]; // 拖动条
+	if(handle == null) return;
 
+	var mouseStart  = {x:0, y:0};  // 鼠标起始位置
+	var elementStart = {x:0, y:0};  // 拖动条起始位置
+
+	handle.onmousedown = function(ev) {
+		var oEvent = ev || event;
+		mouseStart.x  = oEvent.clientX;
+		mouseStart.y  = oEvent.clientY;
+		elementStart.x = element.offsetLeft;
+		elementStart.y = element.offsetTop;
+
+		if (handle.setCapture) {
+			handle.onmousemove = doDrag;
+			handle.onmouseup = stopDrag;
+			handle.setCapture();
+		} else {
+			document.addEventListener("mousemove", doDrag, true);
+			document.addEventListener("mouseup", stopDrag, true);
+		}
+	};
+
+	function doDrag(ev) {
+		var oEvent = ev || event;
+
+		var x = oEvent.clientX - mouseStart.x + elementStart.x;
+		var y = oEvent.clientY - mouseStart.y + elementStart.y;
+		if (x < 0) {
+			x = 0;
+		} else if (x > document.documentElement.clientWidth - element.offsetWidth) {
+			x = document.documentElement.clientWidth - element.offsetWidth;
+		}
+		if (y < 0) {
+			y = 0;
+		} else if (y > document.documentElement.clientHeight - element.offsetHeight) {
+			y = document.documentElement.clientHeight - element.offsetHeight;
+		}
+		element.style.left = x + "px";
+		element.style.top  = y + "px";
+	};
+
+	function stopDrag() {
+		if (handle.releaseCapture) {
+			handle.onmousemove = handle.onmouseup = null;
+			handle.releaseCapture();
+		} else {
+			document.removeEventListener("mousemove", doDrag, true);
+			document.removeEventListener("mouseup", stopDrag, true);
+		}
+	};
 }
-
 
 
 /*********************************** html dom 操作  end **********************************/
