368cfae2c8c1c93e1712e9819ce7e8daae72b062
Jon.King <jinpujun@gmail.com>
2015-04-13 15:59:08 +0800

    relogin and filemanager style improve

--- dm/src/test/java/com/jinhe/dm/report/ScriptParseTest.java
+++ dm/src/test/java/com/jinhe/dm/report/ScriptParseTest.java
@@ -29,15 +29,24 @@ public class ScriptParseTest extends TxTestSupport4DM {
         report1.setType(Report.TYPE1);
         report1.setParentId(Report.DEFAULT_PARENT_ID);
         report1.setName("report-1");
-        report1.setScript(" select id, name from dm_report where ${testMacro} and createTime > ? ");
+        report1.setScript(" select id, name from dm_report where ${testMacro} and createTime > ? " +
+        		" <#if param2 != '-1'> and 1=1 </#if> ");
         
-        String paramsConfig = "[ {'label':'起始时间', 'type':'date', 'nullable':'false'} ]"	;
+        String paramsConfig = "[ " +
+        		"	{'label':'起始时间', 'type':'date', 'nullable':'false'}, " +
+        		"	{'label':'分公司'} " +
+        		"]"	;
         report1.setParam(paramsConfig);
         
         action.saveReport(response, report1);
+        Long reportId = report1.getId();
         
         request.addParameter("param1", "2013-10-01");
-        Long reportId = report1.getId();
+        request.addParameter("param2", "-1");
+        display.showAsJson(request, reportId.toString());
+        
+        request.removeParameter("param2");
+        request.addParameter("param2", "阿斯达");
         display.showAsJson(request, reportId.toString());
     }
     
