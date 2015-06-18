418c0843f9600354beb939604f49b7c981253efa
Jon.King <jinpujun@gmail.com>
2015-06-18 08:37:18 +0800

    完善全文检索Portlet

---
+++ deploy-demo.bat
@@ -0,0 +1,4 @@
+rd /s/q D:\Tomcat7042\webapps\tss
+call mvn clean install  -Pdemo -Dmaven.test.skip=true
+xcopy /Y webapp\target\tss.war  D:\Tomcat7042\webapps
+::start D:\Tomcat7042\bin\startup.bat
